231da5b0c26fb5d254511a3f780a0e42f4744b85
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2023-09-09 23:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+let regexp=/\bthe\b|\ban/b|\ba\b/g:
+
+console.log(band.replace(regexp,$));
+let touristSpots = ['The Virupaksha Temple',
+					'Victoria Memorial', 
+					'Tajmahal'];
 
